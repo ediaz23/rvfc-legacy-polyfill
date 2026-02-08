836db801633105852bfde65202187a19a78406f6
ediaz23 <esteban.diazt23@gmail.com>
2026-02-08 12:09:24 -0300

    [IMP] adapt code for more recent browsers

--- rvfc-legacy-polyfill.js
+++ rvfc-legacy-polyfill.js
@@ -1,31 +1,55 @@
 (function() {
     if (typeof HTMLVideoElement !== 'undefined' &&
         !('requestVideoFrameCallback' in HTMLVideoElement.prototype) &&
-        !('getVideoPlaybackQuality' in HTMLVideoElement.prototype)) {
+        typeof requestAnimationFrame === 'function') {
 
         /** @type {Map<number, Set>} */
         const rvfcHandles = new Map()
+
+        function getPresentedFrames(video) {
+            if (typeof video.getVideoPlaybackQuality === 'function') {
+                try {
+                    const q = video.getVideoPlaybackQuality()
+                    const total = (q && q.totalVideoFrames != null) ? q.totalVideoFrames : 0
+                    const dropped = (q && q.droppedVideoFrames != null) ? q.droppedVideoFrames : 0
+                    return total - dropped
+                } catch (e) { }
+            }
+            return (
+                video.mozPresentedFrames ||
+                video.mozPaintedFrames ||
+                ((video.webkitDecodedFrameCount || 0) - (video.webkitDroppedFrameCount || 0)) ||
+                0
+            )
+        }
 
         /**
          * @this {HTMLVideoElement}
          */
         HTMLVideoElement.prototype.requestVideoFrameCallback = function(cb) {
             let lastTime = this.currentTime
+            let lastPresented = getPresentedFrames(this)
             let rafId = 0
             const dependents = new Set()
 
             const tick = (now) => {
-                if (this.readyState >= 2 && !this.paused && !this.ended) { // is playing
+                if (this.readyState >= 2 && !this.paused && !this.ended) {
                     const ct = this.currentTime
-                    if (ct !== lastTime) { // is new frame
+                    const pf = getPresentedFrames(this)
+
+                    if (pf > lastPresented || ct !== lastTime) {
                         const timediff = now - (this._lastNow || now);
+                        this._lastNow = now
 
                         lastTime = ct
+                        lastPresented = pf
+
                         cb(now, {
                             expectedDisplayTime: now + timediff,
                             width: this.videoWidth || 0,
                             height: this.videoHeight || 0,
                             mediaTime: Math.max(0, ct),
+                            presentedFrames: pf
                         })
                         rvfcHandles.delete(rafId)
                     }
